62cf6bb8971c047b10709f6637e5c30d342df105
myrg15 <merishield@gmail.com>
2023-10-04 12:44:34 +0200

    modified html css js

--- js/storage.js
+++ js/storage.js
@@ -1,22 +1,36 @@
-const circles = document.querySelectorAll('.circle');
+const circles = document.querySelectorAll('.shape');
 const squares = document.querySelectorAll('.square');
 const colorPicker = document.getElementById('colorPicker');
 const chooseButton = document.getElementById('chooseButton');
 const starButton = document.getElementById('starButton');
 
+let selectedShapeSquare = 0;
+const shapeSquares = document.querySelectorAll('.shape-square');
+
+
+
+
 chooseButton.addEventListener('click', () => {
     colorPicker.click();
 });
 
+shapeSquares.forEach((elem,i)=>{
+    elem.addEventListener('click',()=>{
+    selectedShapeSquare=i;
+    })
+})
+
 colorPicker.addEventListener('input', () => {
     const color = colorPicker.value;
+    shapeSquares.item(selectedShapeSquare).style.backgroundColor = color;
+    return;
     squares.forEach((square) => {
         square.style.backgroundColor = color;
     });
 });
 
 starButton.addEventListener('click', () => {
-    circles.forEach((circle) => {
-        circle.style.backgroundColor = colorPicker.value;
+    circles.forEach((shape) => {
+        shape.style.backgroundColor = colorPicker.value;
     });
 });
