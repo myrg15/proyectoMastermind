1fa576f1bfe6df37a8d57236efb2903cf3c5e13b
myrg15 <merishield@gmail.com>
2023-10-05 16:40:11 +0200

    feat: user log session encoding

--- js/storage.js
+++ js/storage.js
@@ -4,33 +4,39 @@ const colorPicker = document.getElementById('colorPicker');
 const chooseButton = document.getElementById('chooseButton');
 const starButton = document.getElementById('starButton');
 
+const username = document.getElementById('username'); 
+
+
 let selectedShapeSquare = 0;
 const shapeSquares = document.querySelectorAll('.shape-square');
 
+//chooseButton.addEventListener('click', () => {
+//    colorPicker.click();
+//});
 
+//shapeSquares.forEach((elem,i)=>{
+  //  elem.addEventListener('click',()=>{
+    //selectedShapeSquare=i;
+    //})
+//})
 
+//colorPicker.addEventListener('input', () => {
+  //  const color = colorPicker.value;
+    //shapeSquares.item(selectedShapeSquare).style.backgroundColor = color;
+    //return;
+    //squares.forEach((square) => {
+      //  square.style.backgroundColor = color;
+   // });
+//});
 
-chooseButton.addEventListener('click', () => {
-    colorPicker.click();
-});
+//starButton.addEventListener('click', () => {
+  //  circles.forEach((shape) => {
+    //    shape.style.backgroundColor = colorPicker.value;
+    //});
+//});
+//LOGIN USUARIO//
 
-shapeSquares.forEach((elem,i)=>{
-    elem.addEventListener('click',()=>{
-    selectedShapeSquare=i;
-    })
-})
-
-colorPicker.addEventListener('input', () => {
-    const color = colorPicker.value;
-    shapeSquares.item(selectedShapeSquare).style.backgroundColor = color;
-    return;
-    squares.forEach((square) => {
-        square.style.backgroundColor = color;
-    });
-});
-
-starButton.addEventListener('click', () => {
-    circles.forEach((shape) => {
-        shape.style.backgroundColor = colorPicker.value;
-    });
+username.addEventListener('input', () =>{
+    localStorage.setItem('username',username.value)
+    console.log(localStorage.getItem('username'));
 });
