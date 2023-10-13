969b447942a63e9da81a888c3bcd7c0c6700c40f
myrg15 <merishield@gmail.com>
2023-10-13 18:54:15 +0200

    feat: debuggin js and html code

--- js/startGame.js
+++ js/startGame.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const userColors = localStorage.getItem('selectedColors');
-    const selectedColors = userColors.split(",");
+const userColors = localStorage.getItem('selectedColors');
+const selectedColors = userColors.split(",");
     if (selectedColors) {
         colorSelect1.value = selectedColors[0];
         colorSelect1.style.backgroundColor = selectedColors[0];
@@ -11,12 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
         colorSelect4.value = selectedColors[3];
         colorSelect4.style.backgroundColor = selectedColors[3];
     }
-    const status0 = document.getElementById("status0");
-    const status1 = document.getElementById("status1");
-    const status2 = document.getElementById("status2");
-    const status3 = document.getElementById("status3");
-    const maquinaColors = localStorage.getItem('maquinaColors');
-    const selectedMaquinaColors = maquinaColors.split(",");
+const status0 = document.getElementById("status0");
+const status1 = document.getElementById("status1");
+const status2 = document.getElementById("status2");
+const status3 = document.getElementById("status3");
+const maquinaColors = localStorage.getItem('maquinaColors');
+const selectedMaquinaColors = maquinaColors.split(",");
 
     if(selectedColors[0] === selectedMaquinaColors[0]){
         status0.style.backgroundColor = "green"
@@ -39,22 +39,15 @@ document.addEventListener("DOMContentLoaded", function() {
         status3.style.backgroundColor = "red"
     }
 
-    const check = document.getElementById("check");
-
+const check = document.getElementById("check");
     check.addEventListener("click", (event) =>{ 
         event.preventDefault(); 
-        /* GUARDO LOS ESTADOS DE LA PRUEBA ACTUAL */
-        /* const bgStatus0 = status0.style.backgroundColor;
-        const bgStatus1 = status1.style.backgroundColor;
-        const bgStatus2 = status2.style.backgroundColor;
-        const bgStatus3 = status3.style.backgroundColor;
- */
 
     /* COMPROBACIÓN DE COLORES Y PUESTA EN EL ESTADO */
-    const machineColors = localStorage.getItem('maquinaColors');
-    const coloresAutogenerados = machineColors.split(",");  
-    const colorAComprobar = [colorSelect1.value, colorSelect2.value, colorSelect3.value, colorSelect4.value];
-    const estados = [status0, status1, status2, status3];  
+const machineColors = localStorage.getItem('maquinaColors');
+const coloresAutogenerados = machineColors.split(",");  
+const colorAComprobar = [colorSelect1.value, colorSelect2.value, colorSelect3.value, colorSelect4.value];
+const estados = [status0, status1, status2, status3];  
 
     for (let i = 0; i < colorAComprobar.length; i++) {       
         if(coloresAutogenerados[i] === colorAComprobar[i]){
@@ -63,120 +56,51 @@ document.addEventListener("DOMContentLoaded", function() {
             estados[i].style.backgroundColor = "red"
         }
     }
-
-   
-
-
-
-   /*  if(coloresAutogenerados[idColorSelect] === colorAComprobar[idColorSelect]){
-    // el estado de idColorSelect lo ponemos en verde
-    switch(idColorSelect){
-        case 0:
-            status0.style.backgroundColor = "green"
-        break; 
-        case 1:
-            status1.style.backgroundColor = "green"
-        break; 
-        case 2:
-            status2.style.backgroundColor = "green"
-        break; 
-        case 3:
-            status3.style.backgroundColor = "green"
-        break;    
-        }
-    } else {
-        // el estado de idColorSelect lo ponemos en rojo
-        switch(idColorSelect){
-            case 0:
-                status0.style.backgroundColor = "red"
-            break; 
-            case 1:
-                status1.style.backgroundColor = "red"
-            break; 
-            case 2:
-                status2.style.backgroundColor = "red"
-            break; 
-            case 3:
-                status3.style.backgroundColor = "red"
-            break;    
-        }
-    } */
-        
-        if(status0.style.backgroundColor === "green" && status1.style.backgroundColor === "green" &&
+       
+    if(status0.style.backgroundColor === "green" && status1.style.backgroundColor === "green" &&
         status2.style.backgroundColor === "green" && status3.style.backgroundColor === "green"){
-                /* alert("has ganado"); */
-                /* TODO: ENVIARLO A LA OTRA PAGINA  */
-                windows.location.href = "winner.html";
+                windows.location.href = "/pages/winner.html";
             } else {
-
-                const divPadre = document.getElementsByClassName('row-select-colors');
-                
+            const divPadre = document.getElementsByClassName('row-select-colors');
                 /* CREACIÓN DE DIVS */
-                const square = createDivWithClass("square");
-                    const palette1 = createDivWithClass("shape-square1", {
-                        backgroundColor: colorSelect1.value
-                    }, "palette1");
-                    const palette2 = createDivWithClass("shape-square1", {
-                        backgroundColor: colorSelect2.value
-                    }, "palette2");
-                    const palette3 = createDivWithClass("shape-square1", {
-                        backgroundColor: colorSelect3.value
-                    }, "palette3");
-                    const palette4 = createDivWithClass("shape-square1", {
-                        backgroundColor: colorSelect4.value
-                    }, "palette4");
+            const square = createDivWithClass("square");
+            const palette1 = createDivWithClass("shape-square1", {
+                backgroundColor: colorSelect1.value
+                }, "palette1");
+            const palette2 = createDivWithClass("shape-square1", {
+                backgroundColor: colorSelect2.value
+                }, "palette2");
+            const palette3 = createDivWithClass("shape-square1", {
+                backgroundColor: colorSelect3.value
+                }, "palette3");
+            const palette4 = createDivWithClass("shape-square1", {
+                backgroundColor: colorSelect4.value
+                }, "palette4");
                 square.appendChild(palette1);
                 square.appendChild(palette2);
                 square.appendChild(palette3);
                 square.appendChild(palette4);
-
-                const track = createDivWithClass("track", {
-                    marginLeft: "230px"
+            const track = createDivWithClass("track", {
+                marginLeft: "230px"
                 }, "trackcombination");
-                    const divStatus0 = createDivWithClass("", {
-                        backgroundColor: status0.style.backgroundColor
-                    }, "status0");
-                    const divStatus1 = createDivWithClass("", {
-                        backgroundColor: status1.style.backgroundColor
-                    }, "status1");
-                    const divStatus2 = createDivWithClass("", {
-                        backgroundColor: status2.style.backgroundColor
-                    }, "status2");
-                    const divStatus3 = createDivWithClass("", {
-                        backgroundColor: status3.style.backgroundColor
-                    }, "status3");
-                    
+            const divStatus0 = createDivWithClass("", {
+                backgroundColor: status0.style.backgroundColor
+                }, "status0");
+            const divStatus1 = createDivWithClass("", {
+                backgroundColor: status1.style.backgroundColor
+                }, "status1");
+            const divStatus2 = createDivWithClass("", {
+                backgroundColor: status2.style.backgroundColor
+                }, "status2");
+            const divStatus3 = createDivWithClass("", {
+                backgroundColor: status3.style.backgroundColor
+                }, "status3");    
                 track.appendChild(divStatus0);
                 track.appendChild(divStatus1);
                 track.appendChild(divStatus2);
                 track.appendChild(divStatus3);
-
                 divPadre[0].insertAdjacentElement('afterend',track);
-                divPadre[0].insertAdjacentElement('afterend',square);
-
-
-
-
-                
-/*                 const nuevoContenido = `
-                <div class="square">
-                    <div class="shape-square1" id="palette1" style="background-color: ${colorSelect1.value};"></div>
-                    <div class="shape-square2" id="palette2" style="background-color: ${colorSelect2.value}"></div>
-                    <div class="shape-square3" id="palette3" style="background-color: ${colorSelect3.value}"></div>
-                    <div class="shape-square4" id="palette4" style="background-color: ${colorSelect4.value}"></div>
-                </div>
-                <div class="track" id="trackcombination" style="margin-left: 230px;">
-                    <div id="status0" style="background-color: ${bgStatus0};">
-                    </div>
-                    <div id="status1" style="background-color: ${bgStatus1};">
-                    </div>
-                    <div id="status2" style="background-color: ${bgStatus2};">
-                    </div>
-                    <div id="status3" style="background-color: ${bgStatus3};">
-                    </div>        
-                </div>
-                `;
-                divPadre[0].insertAdjacentHTML("afterend", nuevoContenido); */
+                divPadre[0].insertAdjacentElement('afterend',square);           
 
                 colorSelect1.value = null;
                 colorSelect1.style.backgroundColor = "white";
@@ -196,199 +120,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
-
 function createDivWithClass(className, styles = {}, id = null) {
     const div = document.createElement('div');
     div.className = className;
-
     if(id){
         div.id = id;
     }
-
     Object.keys(styles).forEach(key => {
         div.style[key] = styles[key];
     });
-
     return div;
 }
-
 function colorChanged(selectElement) {
     const selectedColor = selectElement.value;
     selectElement.style.backgroundColor = selectedColor;
     console.log("Color seleccionado:", selectedColor);
-
 }
-
-
-
-
-
-/* function colorChanged(selectElement) {
-    const selectedColor = selectElement.value;
-    selectElement.style.backgroundColor = selectedColor;
-    console.log("Color seleccionado:", selectedColor);
-
-} */
-
-
-/* const selectedColorsLocal = localStorage.getItem('selectedColor');
-const selectedColors = selectedColorsLocal.split(","); */
-/* const coloresAutogeneradosLOCAL = localStorage.getItem('coloresGeneradosMaquina');
-const coloresAutogenerados = coloresAutogeneradosLOCAL.split(","); */
-/* const status0 = document.getElementById("status0");
-const status1 = document.getElementById("status1");
-const status2 = document.getElementById("status2");
-const status3 = document.getElementById("status3"); */
-
-/* const colorSelect1 = document.getElementById('colorSelect1'); */
-/* colorSelect1.value = selectedColors[0]; */
-/* const palette1 = document.getElementById('palette1');
-const colorSelect2 = document.getElementById('colorSelect2'); */
-/* colorSelect2.value = selectedColors[1]; */
-/* const palette2 = document.getElementById('palette2');
-const colorSelect3 = document.getElementById('colorSelect3'); */
-/* colorSelect3.value = selectedColors[2]; */
-/* const palette3 = document.getElementById('palette3');
-const colorSelect4 = document.getElementById('colorSelect4'); */
-/* colorSelect4.value = selectedColors[3]; */
-/* const palette4 = document.getElementById('palette4');
-let selectedColor = []; */
-
-/* const userColors = localStorage.getItem('selectedColors');
-const selectedColors = userColors.split(",");
-if(selectedColors){
-    colorSelect1.value = selectedColors[0];
-    colorSelect2.value = selectedColors[1];
-    colorSelect3.value = selectedColors[2];
-    colorSelect4.value = selectedColors[3];
-}
-const machineColors = localStorage.getItem('machineColors');
-const coloresAutogenerados = machineColors.split(","); */
-// PALETA DE COLORES
-/* colorSelect1.addEventListener('input', () => {
-    palette1.style.backgroundColor = colorSelect1.value;
-    if (selectedColor[0] === undefined || selectedColor[0] === selectedColor[0]) {
-        selectedColor.splice(0, 1, colorSelect1.value);
-        comprobarEstado(0, colorSelect1.value)
-    } else {
-        selectedColor.splice(0, 1, colorSelect1.value);
-        comprobarEstado(0, colorSelect1.value)
-    }
-})
-colorSelect2.addEventListener('input', () => {
-    palette2.style.backgroundColor = colorSelect2.value;
-    if (selectedColor[1] === undefined || selectedColor[1] === selectedColor[0]) {
-        selectedColor.splice(1, 1, colorSelect2.value);
-        comprobarEstado(1, colorSelect2.value)
-    } else {
-        selectedColor.splice(1, 1, colorSelect2.value);
-        comprobarEstado(1, colorSelect2.value)
-    }
-})
-colorSelect3.addEventListener('input', () => {
-    palette3.style.backgroundColor = colorSelect3.value;
-    if (selectedColor[2] === undefined || selectedColor[2] === selectedColor[0]) {
-        selectedColor.splice(2, 1, colorSelect3.value);
-        comprobarEstado(2, colorSelect3.value)
-    } else {
-        selectedColor.splice(2, 1, colorSelect3.value);
-        comprobarEstado(2, colorSelect3.value)
-    }
-})
-colorSelect4.addEventListener('input', () => {
-    palette4.style.backgroundColor = colorSelect4.value;
-    if (selectedColor[3] === undefined || selectedColor[3] === selectedColor[0]) {
-        selectedColor.splice(3, 1, colorSelect4.value);
-        comprobarEstado(3, colorSelect3.value)
-    } else {
-        selectedColor.splice(3, 1, colorSelect4.value);
-        comprobarEstado(3, colorSelect3.value)
-    }
-}) */
-                                                      
-/* function comprobarEstado(idColorSelect){ */ // con el idColorSelect hacemos referencia al id del input? y con coloraComprobar al que selecciona el jugador
-/* console.log(idColorSelect);  */
-/* console.log(colorAComprobar); */
-/* const colorAComprobar = [colorSelect1.value, colorSelect2.value, colorSelect3.value, colorSelect4.value]; */
-
-/* if(coloresAutogenerados[idColorSelect] === colorAComprobar[idColorSelect]){ */
-    // el estado de idColorSelect lo ponemos en verde
-   /*  switch(idColorSelect){
-        case 0:
-            status0.style.backgroundColor = "green"
-        break; 
-        case 1:
-            status1.style.backgroundColor = "green"
-        break; 
-        case 2:
-            status2.style.backgroundColor = "green"
-        break; 
-        case 3:
-            status3.style.backgroundColor = "green"
-        break;    
-    }
-} else { */
-    // el estado de idColorSelect lo ponemos en rojo
-    /* switch(idColorSelect){
-        case 0:
-            status0.style.backgroundColor = "red"
-        break; 
-        case 1:
-            status1.style.backgroundColor = "red"
-        break; 
-        case 2:
-            status2.style.backgroundColor = "red"
-        break; 
-        case 3:
-            status3.style.backgroundColor = "red"
-        break;    
-    }
-}
-}; */
-
-/* const check = document.getEleme *//* ntById("check"); */
-/*  */
-/* check.addEventListener("click", */ /* (event) =>{ */
-/*     event.preventDefault();  */
-
-    // ¿CUAL ES EL ESTADO DE
-    // trackcombination ?
-  /*   const bgStatus0 = status0.style.backgroundColor;
-    const bgStatus1 = status1.style.backgroundColor;
-    const bgStatus2 = status2.style.backgroundColor;
-    const bgStatus3 = status3.style.backgroundColor;
-
-    if(bgStatus0 === "green" && bgStatus1 === "green" &&
-    bgStatus2 === "green" && bgStatus3 === "green"){
-        alert("has ganado");
-    } else { */
-
-       /*  document.getElementById() */
-        /* const divPadre = document.getElementsByClassName('row-select-colors');
-        const nuevoContenido = `
-        <div class="square">
-            <div class="shape-square1" id="palette1" style="background-color: ${colorSelect1.value};"></div>
-            <div class="shape-square2" id="palette2" style="background-color: ${colorSelect2.value}"></div>
-            <div class="shape-square3" id="palette3" style="background-color: ${colorSelect3.value}"></div>
-            <div class="shape-square4" id="palette4" style="background-color: ${colorSelect4.value}"></div>
-        </div>
-        <div class="track" id="trackcombination" style="margin-left: 230px;">
-            <div id="status0" style="background-color: ${bgStatus0};">
-            </div>
-            <div id="status1" style="background-color: ${bgStatus1};">
-            </div>
-            <div id="status2" style="background-color: ${bgStatus2};">
-            </div>
-            <div id="status3" style="background-color: ${bgStatus3};">
-            </div>        
-        </div>
-        `;
-        divPadre[0].insertAdjacentHTML("afterend", nuevoContenido);
-    }
-})
-comprobarEstado(0);
-comprobarEstado(1);
-comprobarEstado(2);
-comprobarEstado(3); */
-
-
--- js/storage.js
+++ js/storage.js
@@ -11,37 +11,8 @@ const play = document.getElementById('play')
 let selectedColor = [];
 const colores = ["red", "green", "yellow", "pink"];
 
-
-//otra funcion para guardar colores y redireccionar a la pagina de seleccionar 
-/*function gwtRandomHexColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random()*16)];     
-    }
-
-    return color;
-}*/
-
-
-/* function generateRandomCOlorsArray(numColors){
-    const colors = [];
-
-    for (let i = 0; i < numColors; i++) {
-       colors.push(gwtRandomHexColor());   
-    }
-
-    return colors;
-} */
-/* coloresAutogenerados = generateRandomCOlorsArray(4); */
-/* console.log("LA MAQUINA HA CREADO:" + coloresAutogenerados); */
-/* localStorage.setItem('coloresGeneradosMaquina', coloresAutogenerados); */
-
 // PALETA DE COLORES
-
 colorSelect1.addEventListener('input', () => {
-   
     palette1.style.backgroundColor = colorSelect1.value;
     if (selectedColor[0] === undefined || selectedColor[0] === selectedColor[0]) {
         selectedColor.splice(0, 1, colorSelect1.value);
@@ -65,7 +36,7 @@ colorSelect4.addEventListener('input', () => {
         selectedColor.splice(3, 1, colorSelect4.value);
     }
 })
-//reset limpia input regresando color blanco
+//reset input white color
 reset.addEventListener('click', () => {
     selectedColor = [];
     palette1.style.backgroundColor = "#ffffff";
@@ -79,41 +50,29 @@ reset.addEventListener('click', () => {
     console.log(selectedColor);
 })
 play.addEventListener('click', () => {
-   /*  localStorage.setItem('selectedColor', selectedColor); */
-   /*  window.location.href = "/pages/startGame.html"; */
-   getSelectedColors();
+    getSelectedColors();
 })
-
 
 function colorChanged(selectElement) {
     const selectedColor = selectElement.value;
     selectElement.style.backgroundColor = selectedColor;
     console.log("Color seleccionado:", selectedColor);
-
 }
-
-/* NEW */
+/* USER */
 function getSelectedColors() {
-    /* USER */
     const colorSelect1 = document.getElementById("colorSelect1").value;
     const colorSelect2 = document.getElementById("colorSelect2").value;
     const colorSelect3 = document.getElementById("colorSelect3").value;
     const colorSelect4 = document.getElementById("colorSelect4").value;
-
     const selectedColors = [colorSelect1, colorSelect2, colorSelect3, colorSelect4];
-
     localStorage.setItem('selectedColors', selectedColors);
-    
 
-    /* MAQUINA */
-    const maquinaColors = [getRandomColors(),getRandomColors(),getRandomColors(),getRandomColors() ]
-
+    /* MACHINE */
+    const maquinaColors = [getRandomColors(), getRandomColors(), getRandomColors(), getRandomColors()]
     localStorage.setItem('maquinaColors', maquinaColors);
     window.location.href = "/pages/startGame.html";
 }
-
 function getRandomColors() {
-    
     const randomIndex = Math.floor(Math.random() * colores.length);
     return colores[randomIndex];
 }
